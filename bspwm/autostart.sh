a3d1ab2f98f22138b1d9d3bd40027d6766e79b80
AliGigz <ali.gikzg@gmail.com>
2025-05-25 15:09:38 +0330

    Added bspwm and eww widgets

---
+++ bspwm/autostart.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+
+# add command to set a wallpaper
+picom &
+
+## pipewire / uncomment if needed
+#/usr/bin/pipewire &
+#/usr/bin/pipewire-pulse &
+#/usr/bin/pipewire-avb &
+#/usr/bin/pipewire-aes67 &
+#/usr/bin/wireplumber &
+eww daemon &
+eww open bar &
+
